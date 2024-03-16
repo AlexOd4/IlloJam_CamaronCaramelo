48e47f452c9e1e10bdd461f3bce1a714d5edfb48
AlexOd4 <Alexode422@gmail.com>
2024-03-16 21:02:44 +0100

    Implementacion Rutas y estilos

--- scripts/menuManager.gd
+++ scripts/menuManager.gd
@@ -5,7 +5,7 @@ extends Control
 
 
 func _on_button_jugar_pressed():
-	get_tree().change_scene_to_file("res://scenes/menu.tscn")
+	get_tree().change_scene_to_file("res://scenes/dialog.tscn")
 
 
 func _on_button_salir_pressed():
